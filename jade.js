f3187e77c6d565b82278c65614f3f5f673a138e2
Damian Kaczmarek <rush@rushbase.net>
2014-11-26 14:01:26 +0100

    Async loading of compiler and template (for debug)

--- jade.js
+++ jade.js
@@ -2,17 +2,32 @@
 
 var buildMap = {};
 
-var fsXhrSync = {
+var fsXhr = {
   readFileSync : function(fileName, mode) {
     var xhr = new XMLHttpRequest();
     xhr.open("GET",fileName,false);
     xhr.send();
     return xhr.response;
+  },
+  readFile : function(fileName, mode, cb) {
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET",fileName,true);
+    xhr.send();
+    xhr.onreadystatechange = function (aEvt) {
+      if (xhr.readyState == 4) {
+        if(xhr.status == 200) {
+          cb(null, xhr.response);
+        } else {
+          cb(new Error("Got status code " + xhr.status));
+        }
+      }
+    };
+    xhr.onerror = cb;
   }
 };
 
 define('fs.jade', function() {
-  return fsXhrSync;
+  return fsXhr;
 });
 
 var jadeRuntime = (function(exports) {
@@ -272,7 +287,6 @@ define({
     },
     load: function (name, req, onload, config) {
       var url = req.toUrl(name + '.jade');
-
       var fetchText;
       var fs;
       var getCompiler;
@@ -281,28 +295,43 @@ define({
         getCompiler = function(cb) {
           cb(require.nodeRequire('./jade-compiler'));
         }
+        fetchText = function(url, cb) {
+          try {
+            cb(null, fs.readFileSync(url, 'utf8'));
+          } catch(err) {
+            cb(err);
+          }
+        };
       } else{
-        fs = fsXhrSync;
+        fs = fsXhr;
         getCompiler = function(cb) {
           require(['jade-compiler'], function(jadeCompiler) {
             cb(jadeCompiler);
           });
         }
+        fetchText = function(url, cb) {
+          fs.readFile(url, 'utf8', cb);
+        }
       }
-      var fetchText = function(url, cb) {
-        cb(fs.readFileSync(url, 'utf8'));
-      };
       
-      getCompiler(function(jadeCompiler) {
-        fetchText(url, function (text) {
-          var f = jadeCompiler.compile(text, {
-            filename: url
-          });
-          if(!buildMap[name] && config.isBuild) {
-            buildMap[name] = jadeCompiler.compileClient(text, {filename: url});
-          }
-          onload(f);
+      var jadeCompiler, text;
+      function run() {
+        var f = jadeCompiler.compile(text, {
+          filename: url
         });
+        if(!buildMap[name] && config.isBuild) {
+          buildMap[name] = jadeCompiler.compileClient(text, {filename: url});
+        }
+        onload(f);        
+      }
+      getCompiler(function(_jadeCompiler) {
+        jadeCompiler = _jadeCompiler;
+        if(jadeCompiler && text) run();
+      });
+      fetchText(url, function (err, _text) {
+        if(err) throw err;
+        text = _text;
+        if(jadeCompiler && text) run();
       });
     }
   });
